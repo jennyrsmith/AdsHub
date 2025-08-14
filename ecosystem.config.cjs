4fd24eb07634962eb71d516a9210a9f03b66b874
jennyrsmith <157318083+jennyrsmith@users.noreply.github.com>
2025-08-13 21:50:56 -0500

    chore: configure pm2 to run server

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,16 @@
 module.exports = {
-  apps: [{
-    name: 'adshub',
-    script: 'server.js',
-    env: { NODE_ENV: 'production' }
-  }]
+  apps: [
+    {
+      name: 'adshub',
+      script: 'server.js',
+      cwd: '/root/AdsHub',
+      node_args: [],
+      watch: false,
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
 };
